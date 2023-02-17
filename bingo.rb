6186a95753217f426e3dda00827545f6e7391183
nozomi <x8ojo8x@gmail.com>
2023-02-17 12:21:39 +0900

    形はできた

--- bingo.rb
+++ bingo.rb
@@ -1,12 +1,7 @@
 puts "B | I | N | G | O"
 
 
-b = rand(1..15)
-i = rand(16..30)
-n = rand(31..45)
-g = rand(46..60)
-o = rand(61..75)
-number = [b,i,n,g,o]
+
 
 # number.each {|num|
 #   p num
@@ -16,6 +11,29 @@ number = [b,i,n,g,o]
 #   puts "#{b} | #{i} | #{n} | #{g} | #{o}"
 # }
 
-number.each do |num|
-  puts "#{num}".ljust(2) + "|"
+def bingo_num()
+  b = rand(1..15)
+  i = rand(16..30)
+  n = rand(31..45)
+  g = rand(46..60)
+  o = rand(61..75)
+  number = [b,i,n,g,o]
+  number.each do |num|
+    printf "#{num}".rjust(3) + "|"
+  end
+end
+
+5.times {
+
+}
+
+5.times do |time|
+  bingo_num()
+  print "\n"
+
+  # #3回目の3つ目の出力をなくす
+  # if time == 1
+  #   #bingo_num(number[2])
+  #   print "\n"
+  # end
 end
