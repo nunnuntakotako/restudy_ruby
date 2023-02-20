4bb34ffd15a6137f160231f1b60cf92a9928e8a2
nozomi <x8ojo8x@gmail.com>
2023-02-20 10:49:24 +0900

    真ん中の値を空白にした

--- bingo2.rb
+++ bingo2.rb
@@ -1,4 +1,4 @@
-puts "|  B  |  I  |  N  |  G  |  O  | "
+puts "| B  | I  | N  | G  | O  | "
 
 numbers = []
 
@@ -12,30 +12,14 @@ numbers = []
   numbers << number
 end
 
-#２次元配列を作った方が良い？
-# numbers = [number1,number2 ・・・]
-# numbers.each do |number|
-#   number.each do |num|
-#     printf num
-#     if number[2] && num[n]
-#       printf "|  |"
-#     end
-#   end
-# end
-
-
-
-numbers.each do |num|
+numbers.each_with_index do |num,i1|
   printf "|"
-  num.each do |n|
-    # ここを変更する
-    # numbersの3つ目の要素かつnの３つ目の要素が来たら空白を出力したい
-    # if num[2] && n[2]
-    #   printf "      ".rjust(4) + "|"
-    # else
-    #   printf " #{n} ".rjust(4) + "|"
-    # end
-    printf " #{n} ".rjust(4) + "|"
+  num.each_with_index do |n,i2|
+    if i1 == 2 && i2 == 2
+      printf "    ".rjust(4) + "|"
+    else
+      printf " #{n} ".rjust(4) + "|"
+    end
   end
   printf "\n"
 end
