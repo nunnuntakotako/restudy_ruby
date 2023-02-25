ba1662fb8d2841bff8a4ab4e2d68d89766f30bf7
nozomi <x8ojo8x@gmail.com>
2023-02-25 23:23:25 +0900

    done

--- bingo4.rb
+++ bingo4.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 
 bingo_sheet = []
 rand_nums = [
@@ -5,24 +6,20 @@ rand_nums = [
   16..30,
   31..45,
   46..60,
-  61..75,
+  61..75
 ]
 
-puts "| B  | I  | N  | G  | O  | "
+puts '| B  | I  | N  | G  | O  | '
 
 def bingo(rand_num)
-  numbers = Array.new
-  while numbers.length < 5 do
+  numbers = []
+  while numbers.length < 5
     bingo = rand(rand_num)
     numbers << bingo
-    if numbers.length > 1
-      numbers = numbers.uniq
-    end
+    numbers = numbers.uniq if numbers.length > 1
   end
-  return numbers
+  numbers
 end
-
-
 
 (0..4).each do |i|
   bingo_sheet << bingo(rand_nums[i])
@@ -30,33 +27,15 @@ end
 
 bingo_sheet = bingo_sheet.transpose
 
-bingo_sheet.each do |bingos|
-  printf "|"
-  bingos.each do |nums|
-    printf " #{nums} ".rjust(4) + "|"
+bingo_sheet.each_with_index do |bingos, i1|
+  printf '|'
+  bingos.each_with_index do |nums, i2|
+    if i1 == 2 && i2 == 2
+      blank = '    '
+      printf "#{blank.rjust(4)}|"
+    else
+      printf "#{nums.to_s.rjust(4)}|"
+    end
   end
   printf "\n"
 end
-
-
-#number1 => 1~15
-#number2 => 16~30
-#number3 => 31~45 ・・・で作る
-
-#number1から作成する手順
-#リピート内部でrand関数をpushする
-#2要素以上でもし重複していればuniqで取り除く
-
-#5要素 number1.length < 6 となればリピートから抜ける
-#関数を作る num関数(number,b)みたいに
- #第一引数に空の配列number1やnumber2
- #第二引数にrand(1..15)など入れられるか？
-
-#numbers = [number1,number2...,number5]
-#numbersはtransposeさせて縦と横の配列を変える
-#numbers[2]の配列の内部の[2]は" "(空白要素)をpush
-
-# 出力する
-
-# number = number.group_by{|i| i}.reject{|k,v| v.one?}.keys.uniq
-
