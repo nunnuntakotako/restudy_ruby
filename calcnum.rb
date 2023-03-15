08044b0f49c02ca7b9bcc1c3baa8bd90d27e4e38
nozomi <x8ojo8x@gmail.com>
2023-03-15 14:43:04 +0900

    横向きでは合計値を出力できた

--- calcnum.rb
+++ calcnum.rb
@@ -1,26 +1,29 @@
 # frozen_string_literal: true
 
-a = rand(1..10)
-b = rand(11..20)
-c = rand(21..30)
 
-nums = [a,b,c]
+
+element_size = 4
 sum = 0
 sums = []
 nums_box = []
 
-3.times do |time|
-  nums_box << nums
+def make_num
+  a = rand(1..10)
+  b = rand(11..20)
+  c = rand(21..30)
+  nums = [a,b,c]
+  nums
 end
 
-p nums_box
+element_size.times do |time|
+  nums_box << make_num
+end
 
 nums_box.each do |num|
-  num.each do |n|
+  num.each_with_index do |n,i|
     sum += n
+    num << sum if i == 2
   end
-  num << sum
 end
 
-p sum
-p nums_box
+puts nums_box
