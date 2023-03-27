fa1e63f2da8342b77ae743ec03a6f3461bed3906
nozomi <x8ojo8x@gmail.com>
2023-03-27 17:18:44 +0900

    修正

--- calcnum.rb
+++ calcnum.rb
@@ -3,7 +3,6 @@
 element_size = 3
 sum = 0
 resum = 0
-sums = []
 low_sum = []
 nums_box = []
 
@@ -11,18 +10,18 @@ def make_num
   a = rand(1..10)
   b = rand(11..20)
   c = rand(21..30)
-  nums = [a,b,c]
+  nums = [a, b, c]
   nums
 end
 
-element_size.times do |time|
+element_size.times do
   nums_box << make_num
 end
 
 renums_box = nums_box.transpose
 
 nums_box.each do |num|
-  num.each_with_index do |n,i|
+  num.each_with_index do |n, i|
     sum += n
     num << sum if i == 2
   end
@@ -30,7 +29,7 @@ nums_box.each do |num|
 end
 
 renums_box.each do |num|
-  num.each_with_index do |n,i|
+  num.each_with_index do |n, i|
     resum += n
     num << resum if i == 2
   end
