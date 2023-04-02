e4619c6b25878ff4d2668b096ca7ac781fa3b41c
nozomi <x8ojo8x@gmail.com>
2023-04-02 20:50:24 +0900

    とりあえず修正

--- paiza.rb
+++ paiza.rb
@@ -1,13 +1,14 @@
 # 自分の得意な言語で
 # Let's チャレンジ！！
-input_line = gets.to_i
-input_line += 10
-puts input_line
+# input_line = gets.to_i
+# input_line += 10
+# puts input_line
 
 #文字列を囲んで出力
-input_line = gets.to_s
+input_line = gets
 counts = input_line.length
 
-puts "+"
-puts input_line
-puts "+"
+
+puts "+" * (counts + 1)
+print "+#{input_line.concat("+")}"
+puts "+" * (counts + 1)
