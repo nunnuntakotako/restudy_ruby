27ee13f5c2684eaa644c97b66c948f5045a0bbca
nozomi <x8ojo8x@gmail.com>
2023-04-03 09:28:07 +0900

    修正できた

--- paiza.rb
+++ paiza.rb
@@ -1,14 +1,13 @@
-# 自分の得意な言語で
-# Let's チャレンジ！！
-# input_line = gets.to_i
-# input_line += 10
-# puts input_line
-
 #文字列を囲んで出力
 input_line = gets
 counts = input_line.length
+deco = "+" * (counts + 1)
+datas = [deco,"+#{input_line}" ,deco]
 
-
-puts "+" * (counts + 1)
-print "+#{input_line.concat("+")}"
-puts "+" * (counts + 1)
+datas.each_with_index do |data,i|
+  if i == 1
+    puts data.chomp + "+\n"
+  else
+    puts data
+  end
+end
