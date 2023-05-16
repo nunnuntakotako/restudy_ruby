ffe1618b12823b73c1a8780917dbff22765e25ca
nozomi <x8ojo8x@gmail.com>
2023-05-16 23:46:49 +0900

    一時保留

--- sosu.rb
+++ sosu.rb
@@ -5,20 +5,20 @@
 #   num =  num / 2
 # end
 
-def calc_sosu(num)
-  sosu = [11,7,5,3,2]
-  sosu.each do |s|
-    until num < s do
-      unless num < s
-        puts num = num / s
-      end
-    end
-  end
-  if num == 1
-    return num
-  else
-    return "現時点分解不可"
-  end
-end
+# def calc_sosu(num)
+#   sosu = [11,7,5,3,2]
+#   sosu.each do |s|
+#     until num % s == 0 do
+#       unless num < s
+#         puts num = num / s
+#       end
+#     end
+#   end
+#   if num == 1
+#     return num
+#   else
+#     return "現時点分解不可"
+#   end
+# end
 
-calc_sosu(11)
+calc_sosu(15)
