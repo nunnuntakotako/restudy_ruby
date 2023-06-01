1ed5c46c62b62376805a761cd34408b766f4027b
nozomi <x8ojo8x@gmail.com>
2023-06-01 21:42:03 +0900

    久しぶりに素数確認

--- sosu.rb
+++ sosu.rb
@@ -1,24 +1,3 @@
-#素数を求めるプログラム
-# 2 <= num <= 100
+num = 10
 
-# while num < 2 do
-#   num =  num / 2
-# end
-
-# def calc_sosu(num)
-#   sosu = [11,7,5,3,2]
-#   sosu.each do |s|
-#     until num % s == 0 do
-#       unless num < s
-#         puts num = num / s
-#       end
-#     end
-#   end
-#   if num == 1
-#     return num
-#   else
-#     return "現時点分解不可"
-#   end
-# end
-
-calc_sosu(15)
+puts Math.sqrt(num).to_f
